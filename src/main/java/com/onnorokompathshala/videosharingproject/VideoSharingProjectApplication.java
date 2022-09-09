c06672f88814bb95a17d231b91a9a6ff51391cd0
Junayed Rahman <junayed.rahman@g.bracu.ac.bd>
2022-09-09 14:19:16 +0600

    first commit

---
+++ src/main/java/com/onnorokompathshala/videosharingproject/VideoSharingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onnorokompathshala.videosharingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSharingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoSharingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onnorokompathshala/videosharingproject/VideoSharingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onnorokompathshala.videosharingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSharingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
